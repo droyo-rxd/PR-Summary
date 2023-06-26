e321b69b1d05945cae1ef65403cccca25da52a39
droyo-rxd <royosold@moodys.com>
2023-06-26 15:44:18 +0200

    techinical

--- src/prompts.ts
+++ src/prompts.ts
@@ -13,6 +13,8 @@ export const pythonPrompt = (props: {
 }): string =>
   `Write a pull request description focusing on the motivation behind the change and why it improves the project.
     Make it straight to the point.
+    Be as technical as you need.
+    Provide context if needed.
     Add a joke.
     Add some tips for the QAs who are going to test this.
     The title of the pull request is "${props.title}" and the following changes took place: \n ${props.patch_url}`
